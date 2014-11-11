09e496e26b38a2d932acbd97ed4e49c531ac5ec2
Tim Thomas <tim@buyplaytix.com>
2014-11-11 06:49:12 +0400

    checkpoint

--- routes/index.js
+++ routes/index.js
@@ -305,7 +305,7 @@ var smsify = function(str) {
                 response.send('<Response></Response>'); 
             }
             else if (event.state == "off") {
-                response.send('<Response><Sms>Mt Olympus listens. There are no prophecies to determine or prayers to whisper at this time. Keep this tool ready to serve the gods as they instruct.</Sms></Response>');                 
+                response.send('<Response><Sms>Olympus listens. There aren't prophecies or prayers to answer at this time (too fast? too slow?). Keep this tool ready to serve the gods as they instruct.</Sms></Response>');                 
             }
             else if (!testint(body)) {
                 console.log('Bad vote: ' + event.name + ', ' + from + ', ' + body);
