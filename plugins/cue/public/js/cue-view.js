286adae080aabdf7f1fe52248ccf93525fe50a5b
tthomas48 <tim@buyplaytix.com>
2015-01-02 01:19:15 +0000

    made images nicer

--- plugins/cue/public/js/cue-view.js
+++ plugins/cue/public/js/cue-view.js
@@ -22,8 +22,8 @@ deus.sprite = (function($, createjs, undefined) {
       
       if (this.imagePath) {
         this.bitmap = new createjs.Bitmap("/plugin/cue/images/" + this.imagePath);
-        this.bitmap.scaleX = this.totalOptions == 3 ? 0.75 : 1;
-        this.bitmap.scaleY = this.totalOptions == 3 ? 0.75 : 1;
+        this.bitmap.scaleX = this.totalOptions == 3 ? 0.75 : 0.7;
+        this.bitmap.scaleY = this.totalOptions == 3 ? 0.75 : 0.7;
         stage.addChild(this.bitmap);
       }
 
@@ -32,9 +32,11 @@ deus.sprite = (function($, createjs, undefined) {
         var bounds = that.bitmap.getTransformedBounds();
         if (that.position === 1) {
           that.bitmap.x = 0;
+          that.bitmap.y = 250; 
         }
         else if (that.position === 2) {
           that.bitmap.x = stage.canvas.width - bounds.width - 50;
+          that.bitmap.y = 250; 
         }
         else if (that.position === 3) {
           that.bitmap.x = (stage.canvas.width - bounds.width) / 2;
@@ -42,9 +44,12 @@ deus.sprite = (function($, createjs, undefined) {
         }
         // 150 is the amount from the top, that the stage starts
         // 
-        if (that.totalOptions == 3 && (that.position == 1 || that.position == 2)) {
-          that.bitmap.y = (stage.canvas.height - bounds.height) / 2; 
-        }
+//         if (that.totalOptions == 3 && (that.position == 1 || that.position == 2)) {
+//           that.bitmap.y = (stage.canvas.height - bounds.height) / 2; 
+//         }
+//         else {
+//           that.bitmap.y = 250; 
+//         }
 
         //that.bitmap.y = (stage.canvas.height - bounds.height) / 2;
       };
