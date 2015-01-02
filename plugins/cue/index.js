bca4c3713698a83c9c5991a6048cdfd3f1ea4cbc
tthomas48 <tim@buyplaytix.com>
2015-01-02 00:40:59 +0000

    reduce flickr

--- plugins/cue/index.js
+++ plugins/cue/index.js
@@ -182,14 +182,17 @@ function cue(name, deps) {
       console.log("cue set", cmd);
       nextCue = cmd.cue;
       setCue(String(cmd.cue));
-      
-      deps.io.sockets.emit('clearOlympus');
       if (!cmd.go) {
         cmd.go = 'go';
       }
       else if (cmd.go == 'vote' && cueNumber != cmd.cue) {
+        deps.io.sockets.emit('clearOlympus');
         // jump to the cue
         cueNumber = String(cmd.cue);
+      }
+      
+      if (cmd.go == 'go') {
+        deps.io.sockets.emit('clearOlympus');
       }
       
       emitStatus(deps, cmd.go);
